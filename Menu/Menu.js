a8a13bbae45007025ecdeaf71b61de2c51b3e5ab
Andrew Fox <andrewbfox@gmail.com>
2019-06-19 19:36:32 -0400

    Modified menu.js to stop propagation of click on menu button; modified index.css to attempt to animate menu slideout

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,4 +9,8 @@ const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", (event) => event.stopPropagation());
 menuButton.addEventListener("click", toggleMenu);
+
+// Trying to get menu to slide out when click occurs anywhere in the window:
+// window.addEventListener("click", toggleMenu);
